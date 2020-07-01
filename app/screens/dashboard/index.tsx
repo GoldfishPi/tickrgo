f6a250b75fba93bd0e9a3ba3cd4ddc47bb083dfe
Erik Badger <erikbadger777@gmail.com>
2020-07-01 09:51:11 -0500

    test lib-bi fetch

--- app/screens/dashboard/index.tsx
+++ app/screens/dashboard/index.tsx
@@ -1,9 +1,28 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {Text} from 'react-native';
+import {useApi} from '../../util/providers/ApiProvider';
 
 interface DashboardScreensProps {}
 
 const DashboardScreens: FC<DashboardScreensProps> = ({}) => {
+    const {api} = useApi();
+    useEffect(() => {
+        console.log('am here');
+        console.log('api', api.defaults.headers.common.Authorization);
+        api.post('/bi/trends', {
+            filters: {
+                dates: 'now-7d/d',
+            },
+            options: {
+                mixins: [{type: 'newsVolume'}],
+                obj:true,
+            },
+        })
+            .then((res) => {
+                console.log('got data lol', JSON.stringify(res.data));
+            })
+            .catch((e) => console.log('fail :(', e));
+    }, [api]);
     return <Text>Dashboard</Text>;
 };
 
