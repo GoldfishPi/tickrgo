fc9c8dc3f23b5e20f694e9e3df8b52c30b8f1da9
Erik Badger <erikbadger777@gmail.com>
2020-07-01 13:14:06 -0500

    enabled dark theme for my eyes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Screens from './app/screens';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+    NavigationContainer,
+    DarkTheme,
+} from '@react-navigation/native';
 import {TickrProviders} from './app/util';
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
     return (
         <TickrProviders>
-            <NavigationContainer>
+            <NavigationContainer
+                theme={{
+                    ...DarkTheme,
+                }}>
                 <Screens />
             </NavigationContainer>
         </TickrProviders>
