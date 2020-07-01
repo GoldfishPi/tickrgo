243d3c298ebc98a33cb47fb638fbed7773492158
Erik Badger <erikbadger777@gmail.com>
2020-07-01 10:36:01 -0500

    added news sparkline

--- app/screens/dashboard/index.tsx
+++ app/screens/dashboard/index.tsx
@@ -1,17 +1,20 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Text} from 'react-native';
 import {useApi} from '../../util/providers/ApiProvider';
+import {View} from 'native-base';
+import {BarChart, Grid} from 'react-native-svg-charts';
+import * as shape from 'd3-shape';
 
 interface DashboardScreensProps {}
 
 const DashboardScreens: FC<DashboardScreensProps> = ({}) => {
     const {api} = useApi();
+
+    const [data, setData] = useState<number[]>([]);
     useEffect(() => {
-        console.log('am here');
-        console.log('api', api.defaults.headers.common.Authorization);
         api.post('/bi/trends', {
             filters: {
-                dates: 'now-7d/d',
+                dates: 'now-90d/d',
             },
             options: {
                 mixins: [{type: 'newsVolume'}],
@@ -20,10 +23,23 @@ const DashboardScreens: FC<DashboardScreensProps> = ({}) => {
         })
             .then((res) => {
                 console.log('got data lol', JSON.stringify(res.data));
+                setData(res.data.newsVolume.primary.map(({val}: any) => val));
             })
             .catch((e) => console.log('fail :(', e));
     }, [api]);
-    return <Text>Dashboard</Text>;
+    return (
+        <View>
+            <Text>DashboardScreens</Text>
+            <BarChart
+                style={{height: 200}}
+                data={data}
+                contentInset={{top: 30, bottom: 30}}
+                curve={shape.curveNatural}
+                svg={{fill: 'rgba(134, 65, 244, 0.8)'}}>
+                <Grid />
+            </BarChart>
+        </View>
+    );
 };
 
 export default DashboardScreens;
