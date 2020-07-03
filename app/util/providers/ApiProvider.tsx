f40075c09d8e4a6b9174ac6a5e13856a7ebc4b09
Erik Badger <erikbadger777@gmail.com>
2020-07-03 13:24:01 -0500

    removed commented out code

--- app/util/providers/ApiProvider.tsx
+++ app/util/providers/ApiProvider.tsx
@@ -1,31 +1,11 @@
 import axios from 'axios';
 import {useEnv} from './EnvProvider';
 
-// interface ApiState {
-//     api: AxiosInstance;
-// }
-
-// export const ApiContext = createContext<ApiState>({
-//     api: axios,
-// });
-
-// export const ApiProvider: React.FC = ({children}) => {
-//     const {apiUrl} = useEnv();
-
-//     const state = {
-//         api,
-//     };
-
-//     return <ApiContext.Provider value={state}>{children}</ApiContext.Provider>;
-// };
-
 export const useApi = () => {
     const {apiUrl, apiToken} = useEnv();
-    console.log('got stuff??', apiToken);
     const api = axios.create({
         baseURL: apiUrl,
         headers: {
-            // Authorization: apiToken,
             common: {
                 Authorization: apiToken,
             },
