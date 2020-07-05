3d6ee70082091e28e5be00b491f51f1a85e5e5cc
Erik Badger <erikbadger777@gmail.com>
2020-07-05 17:30:15 -0500

    added filter dialog to home screen

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import {createStackNavigator} from '@react-navigation/stack';
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {Appbar} from 'react-native-paper';
+import {Appbar, Dialog, Text} from 'react-native-paper';
 import CardReport from './CardReport';
 import Overview from './Overview';
+import {View} from 'react-native';
+import FilterDialog from 'app/components/FilterDialog';
 
 interface DashboardScreensProps {}
 
@@ -28,48 +30,58 @@ const Stack = createStackNavigator<HomeStackParamList>();
 
 const DashboardScreens: FC<DashboardScreensProps> = ({}) => {
     const {t} = useTranslation();
+    const [filtersDialogActive, setFiltersDialogActive] = useState(false);
     return (
-        <Stack.Navigator
-            screenOptions={{
-                header: ({previous, scene, navigation}) => (
-                    <Appbar.Header accessibilityStates={{}}>
-                        {previous ? (
-                            <Appbar.BackAction
+        <View style={{flex: 1}}>
+            <Stack.Navigator
+                screenOptions={{
+                    header: ({previous, scene, navigation}) => (
+                        <Appbar.Header accessibilityStates={{}}>
+                            {previous ? (
+                                <Appbar.BackAction
+                                    accessibilityStates={{}}
+                                    onPress={navigation.goBack}
+                                />
+                            ) : (
+                                <Appbar.Action
+                                    accessibilityStates={{}}
+                                    icon="menu"
+                                    onPress={() => {}}
+                                />
+                            )}
+                            <Appbar.Content
                                 accessibilityStates={{}}
-                                onPress={navigation.goBack}
+                                title={
+                                    scene.descriptor.options.title
+                                        ? scene.descriptor.options.title
+                                        : scene.route.name
+                                }
                             />
-                        ) : (
-                            <Appbar.Action
-                                accessibilityStates={{}}
-                                icon="menu"
-                                onPress={() => {}}
-                            />
-                        )}
-                        <Appbar.Content
-                            accessibilityStates={{}}
-                            title={
-                                scene.descriptor.options.title
-                                    ? scene.descriptor.options.title
-                                    : scene.route.name
-                            }
-                        />
-                        {!previous && (
-                            <Appbar.Action
-                                accessibilityStates={{}}
-                                icon="filter-variant"
-                                onPress={() => {}}
-                            />
-                        )}
-                    </Appbar.Header>
-                ),
-            }}>
-            <Stack.Screen name="Home" component={Overview} />
-            <Stack.Screen
-                name="CardReport"
-                options={{title: t('Card Report')}}
-                component={CardReport}
-            />
-        </Stack.Navigator>
+                            {!previous && (
+                                <Appbar.Action
+                                    accessibilityStates={{}}
+                                    icon="filter-variant"
+                                    onPress={() =>
+                                        setFiltersDialogActive(
+                                            (active) => !active,
+                                        )
+                                    }
+                                />
+                            )}
+                        </Appbar.Header>
+                    ),
+                }}>
+                <Stack.Screen name="Home" component={Overview} />
+                <Stack.Screen
+                    name="CardReport"
+                    options={{title: t('Card Report')}}
+                    component={CardReport}
+                />
+            </Stack.Navigator>
+            <FilterDialog
+                visible={filtersDialogActive}
+                onDismiss={() => setFiltersDialogActive(false)}/>
+        </View>
     );
 };
 
