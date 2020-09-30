4c68ef1958adde18714364acf7e21780904c19bf
Erik Badger <erikbadger777@gmail.com>
2020-09-30 11:57:03 -0500

    fixed bi hooks

--- app/util/hooks/libBi.ts
+++ app/util/hooks/libBi.ts
@@ -1,7 +1,7 @@
 import {RequestOptions, SearchFilters, ParsedObject} from 'lib-bi';
 import {useApi} from './Api';
 import {Trend} from 'lib-bi/dist/models/trends/types';
-import {useState, useEffect, useMemo} from 'react';
+import {useState} from 'react';
 
 interface Request {
     options: RequestOptions;
@@ -31,12 +31,8 @@ const useBi = <t>(
     return [fetchedData, fetch];
 };
 
-const useTrends = (filters: SearchFilters, body: RequestOptions) => {
-    useEffect(() => {
-        console.log('this will be where i fetch', filters);
-    }, [filters, body]);
-    // return useBi<Trend>(body, 'trends');
-    return [{}, () => new Promise(() => {})];
+const useTrends = (body: Request) => {
+    return useBi<Trend>(body, 'trends');
 };
 
 const useCards = (
